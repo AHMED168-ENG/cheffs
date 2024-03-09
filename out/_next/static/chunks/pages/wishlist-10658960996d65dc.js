68bf88c9bc941c7eca8dd04bc34aebe649f287bc
ahmed zaky <aalsaeed@nadeer.com.sa>
2024-03-09 22:35:41 +0200

    updates

---
+++ out/_next/static/chunks/pages/wishlist-10658960996d65dc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4737],{36686:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/wishlist",function(){return u(51114)}])}},function(n){n.O(0,[2888,9774,179],function(){return n(n.s=36686)}),_N_E=n.O()}]);
